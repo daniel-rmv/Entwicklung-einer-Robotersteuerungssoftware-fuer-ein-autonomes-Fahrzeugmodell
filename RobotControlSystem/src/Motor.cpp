24924ea4773bca395cbf217e6349ac9cdd4355f6
daniel.rmv <daniel.wuermli@icloud.com>
2025-01-23 10:06:48 +0100

    MIT Lizenz aktualisiert

--- RobotControlSystem/src/Motor.cpp
+++ RobotControlSystem/src/Motor.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 Daniel Würmli
+ *
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+
 #include "Motor.h"
 #include <wiringPiI2C.h>
 #include <iostream>
--- RobotControlSystem/src/NavigationSystem.cpp
+++ RobotControlSystem/src/NavigationSystem.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 Daniel Würmli
+ *
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+
 #include "NavigationSystem.h"
 #include <iostream>
 #include <unistd.h>
